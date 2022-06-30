fe96cc9e98061a9bf9a5b78c3b149007a4905666
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-30 09:17:22 -0600

    Add files via upload

---
+++ cypress/integration/exchange/send.test.ts
@@ -0,0 +1,16 @@
+describe('Send', () => {
+  it('should redirect', () => {
+    cy.visit('/send')
+    cy.url().should('include', '/swap')
+  })
+
+  it('should redirect with url params', () => {
+    cy.visit(
+      '/send?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x39Af062b155978f1D41B299601DeFac54E94Cbd8',
+    )
+    cy.url().should(
+      'contain',
+      '/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x39Af062b155978f1D41B299601DeFac54E94Cbd8',
+    )
+  })
+})
